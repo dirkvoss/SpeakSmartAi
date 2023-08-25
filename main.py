d181250e691325cb46de1911d82939eee7fe6dd2
dirkvoss <mail@dirk-voss.com>
2023-08-25 17:43:35 +0200

    NewVersion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "OPENAI_API_KEY"
 chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Wann war der 1 Weltkreig?"}])
 response = chat_completion.choices[0].message["content"]
 print(response)
